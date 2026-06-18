b7e504f933d791805c25d68c66f57466a6801ecf
Vivek Lakum <lakumvivek01@gmail.com>
2026-06-18 13:00:48 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -16,10 +16,7 @@ class IngredientRequest(BaseModel):
 # ---------------------------
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
