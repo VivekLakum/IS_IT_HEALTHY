e2003c5352e19b4c929b7c2add422e78924326fe
Vivek Lakum <lakumvivek01@gmail.com>
2026-06-20 02:04:40 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import re
-from matplotlib import category
 
 # ---------------------------
 # FastAPI App
