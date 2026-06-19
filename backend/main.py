a924084cbb3a6fcebda07a0bff8a537d2e517c7c
Vivek Lakum <lakumvivek01@gmail.com>
2026-06-20 01:46:52 +0530

    Restrict CORS to Vercel app origins
    Updated CORS middleware to restrict origins to Vercel apps.

--- backend/main.py
+++ backend/main.py
@@ -33,7 +33,9 @@ class ManualNutritionRequest(BaseModel):
 # ---------------------------
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    # allow_origins=["*"],
+    allow_origin_regex=r"https://.*\.vercel\.app",
+
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
