57852d637c98072a0cb371d2bceef6d7d95c13e3
Vivek Lakum <lakumvivek01@gmail.com>
2026-06-20 01:43:45 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -33,7 +33,7 @@ class ManualNutritionRequest(BaseModel):
 # ---------------------------
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://labeliq-lovat.vercel.app"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
